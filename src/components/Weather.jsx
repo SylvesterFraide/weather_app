e1122a04592bfee06ef8a902c4c1343f75edca68
SylvesterFraide <fridaysylvester2@gmail.com>
2025-08-21 14:26:39 +0300

    6th commit

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Weather.css";
 import search_icon from "../assets/search.jpg";
 import clear from "../assets/clear.png";
@@ -11,6 +11,51 @@ import humidity from "../assets/humidity.jpg";
 import SearchTwoToneIcon from "@mui/icons-material/SearchTwoTone";
 
 const Weather = () => {
+  const [weatherData, setWeatherData] = useState(false);
+
+  const allIcons = {
+    "01d": clear,
+    "01n": clear,
+    "02d": cloud,
+    "02n": cloud,
+    "03d": cloud,
+    "03n": cloud,
+    "04d": drizzle,
+    "04n": drizzle,
+    "09d": rain,
+    "09n": rain,
+    "10d": rain,
+    "10n": rain,
+    "13d": snow,
+    "13n": snow,
+  };
+
+  const search = async (city) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
+        import.meta.env.VITE_APP_ID
+      }`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+
+      console.log(data);
+      setWeatherData({
+        temperature: Math.floor(data.main.temp),
+        location: data.name,
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        icon: data.weather[0].icon,
+      });
+
+      console.log(data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    search("London");
+  }, []);
+
   return (
     <div className="weather">
       <div className="search-bar">
